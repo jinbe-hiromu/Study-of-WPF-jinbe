32c3df585f1377080304d00734fa1d4019b11ca4
HIROMU MATSUBARA <hiromu.matsubara.j2x@denso-wave.com>
2020-12-09 15:21:33 +0900

    HelloWorld First CheckIn

---
+++ HelloWorld/HelloWorld/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Reactive.Bindings;
+
+namespace HelloWorld
+{
+    class MainViewModel
+    {
+        MainModel model = new MainModel();
+
+        public ReactiveProperty<string> Text { get; private set; }
+
+        public object TextRead(object dataContext)
+        {
+            this.Text = model.Text;
+            dataContext = this;
+            return dataContext;
+        }
+
+    }
+}
